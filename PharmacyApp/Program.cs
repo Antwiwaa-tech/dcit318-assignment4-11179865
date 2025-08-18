f04096ee0124ddf5b5165ec2405403024c7b1292
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-08-18 19:29:31 +0000

    main program

---
+++ PharmacyApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PharmacyApp
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Start with the Main Form
+            Application.Run(new MainForm());
+        }
+    }
+}
